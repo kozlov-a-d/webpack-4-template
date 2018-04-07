5d7fde58c96feb1531da425351964542007122df
Andrey Kozlov <andreydmitrievich666@gmail.com>
2018-04-07 11:49:36 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+warning webpack-cli > jscodeshift > babel-preset-es2015@6.24.1: 🙌  
+Thanks for using Babel: we recommend using babel-preset-env now: please read babeljs.io/env to update!
+
+https://habrahabr.ru/post/350886/  
+И самый спорный момент. Для пакета css-loader мы добавили параметр url...
+
+
+https://habrahabr.ru/company/skbkontur/blog/351080/
+как ускорить сборку
+
+
+1. проверить пути к картинкам
+2. добавить твиг
+3. добавить оптимизацию картинок
+4. посмотреть статью про ускорение билда
+5. авторелоад
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+const myPath = {
+    scripts: {
+        entry: './src/assets/scripts/index.js',
+        resolve: 'src/assets/scipts',
+        output: './assets/build/app.js'
+    },
+    styles: {
+        entry: './src/assets/styles/main.scss',
+        resolve: 'src/assets/styles',
+        output: './assets/build/app.css'
+    },
+    html: {
+        entry: './src/html/views',
+        resolve: 'src/html/includes',
+    },
+    dist: 'dist'
+}
+
+function generateHtmlPlugins(templateDir) {
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map(item => {
+        const parts = item.split('.');
+        const name = parts[0];
+        const extension = parts[1];
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+            inject: false,
+        })
+    })
+}
+
+const htmlPlugins = generateHtmlPlugins(myPath.html.entry);
+
+module.exports = {
+    entry: [
+        myPath.scripts.entry,
+        myPath.styles.entry,
+    ],
+    output: {
+        filename: myPath.scripts.output
+    },
+    devtool: "source-map",
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, myPath.scripts.resolve),
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: 'env'
+                }
+            }
+        }, {
+            test: /\.(sass|scss)$/,
+            include: path.resolve(__dirname, myPath.styles.resolve),
+            use: ExtractTextPlugin.extract({
+                use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            minimize: true,
+                            url: false
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            })
+        }, {
+            test: /\.html$/,
+            include: path.resolve(__dirname, myPath.html.resolve),
+            use: ['raw-loader']
+        }, ]
+    },
+    plugins: [
+        // new CleanWebpackPlugin([myPath.dist]),
+        new ExtractTextPlugin({
+            filename: myPath.styles.output,
+            allChunks: true,
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/assets/fonts',
+            to: './assets/fonts'
+          },
+          {
+            from: './src/assets/favicon',
+            to: './assets/favicon'
+          },
+          {
+            from: './src/assets/images',
+            to: './assets/images'
+          },
+          {
+            from: './src/assets/uploads',
+            to: './assets/uploads'
+          }
+        ]),
+    ].concat(htmlPlugins)
+};
